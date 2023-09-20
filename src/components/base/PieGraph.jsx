31f58b8627f46399be867e88f82f23d2919a124d
hactuallybenji <mrbenamiller@gmail.com>
2023-09-20 12:20:33 -0700

    Add PieGraph component

---
+++ src/components/base/PieGraph.jsx
@@ -0,0 +1,7 @@
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
+
+function PieGraph({ data, colors, label, categoryToggle, columnsSpan }) {
+  return ();
+}
+
+export default PieGraph;
