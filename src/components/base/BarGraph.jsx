e68e2186ec59ae06063e291cd56b29e3939c206d
hactuallybenji <mrbenamiller@gmail.com>
2023-09-20 12:23:14 -0700

    Add BarGraph.jsx

---
+++ src/components/base/BarGraph.jsx
@@ -0,0 +1,5 @@
+import { BarChart, Bar, XAxis, Tooltip, ResponsiveContainer, YAxis } from 'recharts';
+
+function BarGraph ({ data, totalValue, label, categoryToggle, columnsSpan }) {
+  return ();
+}
