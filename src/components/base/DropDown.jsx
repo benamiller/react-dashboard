0aec3b2876dc0eab7b590b46525e681c22fff5bd
hactuallybenji <mrbenamiller@gmail.com>
2023-09-20 12:33:39 -0700

    Conditionally use parent state

--- src/components/base/DropDown.jsx
+++ src/components/base/DropDown.jsx
@@ -3,6 +3,11 @@ import { ListBox, Transition } from '@headlessui/react';
 import { CheckIcon, SelectorIcon } from '@headlessui/react';
 
 function DropDown({ list, parentStateSelect, setParentStateSelect }) {
+  const [selfStateSelected, setSelfStateSelected] = useState(list[0]);
+
+  const selected = parentStateSelected == null ? selfStateSelected : parentStateSelect;
+  const setSelected = setParentStateSelect == null ? setSelfStateSelected : setParentStateSelect;
+
   return ();
 }
 
