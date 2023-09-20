e1e84468ecfeac5d2d5c744050ecbd59e2a5a703
hactuallybenji <mrbenamiller@gmail.com>
2023-09-19 19:33:43 -0700

    Add rechart dependency && AreaGraph component

---
+++ src/components/base/AreaGraph.jsx
@@ -0,0 +1,7 @@
+import { ResponsiveContainer, AreaChart, Tooltip, XAxis, YAxis, Area } from "recharts";
+
+function AreaGraph({ data, totalValue, label, categoryToggle, columnsSpan }) {
+  return ();
+}
+
+export default AreaGraph;
