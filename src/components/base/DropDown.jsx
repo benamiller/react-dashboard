653f5f79ae0b02979efd8aa657a201218fd80ae1
hactuallybenji <mrbenamiller@gmail.com>
2023-09-20 12:30:08 -0700

    Adds headlessui DropDown

---
+++ src/components/base/DropDown.jsx
@@ -0,0 +1,9 @@
+import { Fragment, useState } from 'react';
+import { ListBox, Transition } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@headlessui/react';
+
+function DropDown({ list, parentStateSelect, setParentStateSelect }) {
+  return ();
+}
+
+export default DropDown;
